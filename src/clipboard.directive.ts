d5f7c299bd18f77f5a92858637e66bf7e70b38b1
Sam Lin <maxisam@gmail.com>
2017-04-03 14:24:19 -0500

    fix(AOT): should set prop as public
    AoT can't access private prop
    
    #39

--- src/clipboard.directive.ts
+++ src/clipboard.directive.ts
@@ -9,19 +9,19 @@ export { Clipboard };
 export class ClipboardDirective implements OnInit, OnDestroy {
     public clipboard: Clipboard;
 
-    @Input('ngxClipboard') private targetElm: ElementRef;
+    @Input('ngxClipboard') public targetElm: ElementRef;
 
-    @Input() private cbContent: string;
+    @Input() public cbContent: string;
 
-    @Output() private cbOnSuccess: EventEmitter<boolean> = new EventEmitter<boolean>();
+    @Output() public cbOnSuccess: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-    @Output() private cbOnError: EventEmitter<boolean> = new EventEmitter<boolean>();
+    @Output() public cbOnError: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-    constructor(private elmRef: ElementRef) { }
+    constructor(public elmRef: ElementRef) { }
 
     public ngOnInit() {
         let option: Clipboard.Options;
-        option = !!this.targetElm ? { target: () => <any> this.targetElm } : { text: () => this.cbContent };
+        option = !!this.targetElm ? { target: () => <any>this.targetElm } : { text: () => this.cbContent };
         this.clipboard = new Clipboard(this.elmRef.nativeElement, option);
         this.clipboard.on('success', () => this.cbOnSuccess.emit(true));
         this.clipboard.on('error', () => this.cbOnError.emit(true));
